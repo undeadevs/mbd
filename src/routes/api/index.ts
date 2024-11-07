0043897e021a20f0e18b0253a2b58c6888f5d429
Ilham <jangantanyaaku14@gmail.com>
2024-11-07 08:49:37 +0800

    fix: import naming monstersRouter

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
 import { authRouter } from "@/routes/api/auth";
-import { monsterRouter } from "./monsters";
+import { monstersRouter } from "./monsters";
 
 const router = new Hono();
 
 router.route("/auth", authRouter);
-router.route("/monsters", monsterRouter);
+router.route("/monsters", monstersRouter);
 
 export { router as apiRouter };
